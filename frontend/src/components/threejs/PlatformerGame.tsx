dd483441a5a1ea5c6ddf4f187776f74a11987539
ezrahuffman <joshooha@gmail.com>
2025-06-01 15:27:09 -0700

    fix floor

--- frontend/src/components/threejs/PlatformerGame.tsx
+++ frontend/src/components/threejs/PlatformerGame.tsx
@@ -24,8 +24,8 @@ const Platform = ({ position, width = 3, height = 1, winning = false} : Platform
 // Floor component
 const Floor = () => {
   return (
-    <mesh position={[0, -4, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <boxGeometry args={[20, 10, 1]} />
+    <mesh position={[0, -5, 0]} rotation={[0, 0, 0]}>
+      <boxGeometry args={[20, 1, 1]} />
       <meshStandardMaterial color={platformColor} />
     </mesh>
   );
